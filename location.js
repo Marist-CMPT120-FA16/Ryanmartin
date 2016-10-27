959b315c6864f3395632857a3a4cf4d0730def1b
ryanmartin14 <ryan.martin3@marist.edu>
2016-10-27 16:23:08 -0400

    Add files via upload

---
+++ location.js
@@ -0,0 +1,50 @@
+
+	function start(){
+		var description = "You are hungry in the suburbs and you need to find food. You want to eat pizza.";
+		return description;
+	}
+
+	function street(){
+		var description = "You are walking up the street.";
+		return description;
+	}
+	
+	function dirtroad(){
+		var description = "You cut down a dirt road for a shortcut.";
+		return description;
+	}
+	
+	function shortcut(){
+		var description = "The shortcut leads you to a pizza parlor.";
+		return description;
+	}
+	
+	function pizzaparlor(){ 
+		var description	= "You enter the pizza parlor and now enojy delicious pizza. You acquire pizza to your inventory.";
+		return description;
+	}
+	
+	function outsidepizzaparlor(){
+		var description = "Unfortunately the Pizza did not fill you up. You are still hungry you must find more food. You leave the pizza parlor.";
+		return description;
+	}
+
+	function tunnel(){
+		var description = "You walk down the street and come across a tunnel you walk down it.";
+		return description;
+	}
+
+	function bridge(){ 
+		var description	= "The tunnel leads you to a bridge you walk over it.";
+		return description;	
+	}
+
+	function cornfield(){ 
+		var description	= "The bridge leads to a corn field. You start to eat the corn on the field. You take some corn with you.";
+		return description;
+	}
+
+	function river(){
+		var description = "You finish eating the corn and are still hungry. The field has led you to a river. You acquire fresh water to drink.";
+		return description;
+	}
